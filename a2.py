1ea2c1623a32dc98742cbdb048f3435db858a371
Catherine Markowska <cimarkowska@cps.edu>
2023-09-20 10:42:54 -0500

    a2 practice

--- a2.py
+++ a2.py
@@ -29,19 +29,25 @@ def match(pattern: List[str], source: List[str]) -> List[str]:
         # 1) if we reached the end of the pattern but not source    
         if pind == len(pattern) and sind < len(source):
             return None
+            ## .join 
 
         # 2) if the current thing in the pattern is a %
         # WARNING: this condition contains the bulk of the code for the assignment
         # If you get stuck on this one, we encourage you to attempt the other conditions
         #   and come back to this one afterwards
+        elif pattern[pind] == "%":
+            return None 
 
         # 3) if we reached the end of the source but not the pattern
         elif sind == len(source) and pind < len(pattern):
             return None 
 
         # 4) if the current thing in the pattern is an _
-        elif "_" in pattern[pind]:
-            len(source)
+        elif pattern[pind] == "_":
+            # append to the result list 
+            result.append(source[sind])
+            pind += 1 
+            sind += 1
 
         # 5) if the current thing in the pattern is the same as the current thing in the
         # source start this one
@@ -49,7 +55,7 @@ def match(pattern: List[str], source: List[str]) -> List[str]:
             pind += 1 
             sind += 1
 
-
+ 
         # 6) else : this will happen if none of the other conditions are met it
         # indicates the current thing it pattern doesn't match the current thing in
         # source
